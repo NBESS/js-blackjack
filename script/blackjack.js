350c97209cb4bf41dc43e8ac2fe0d17f3d17e3be
NBESS <nicholasdbess@gmail.com>
2020-04-23 19:23:06 -0400

    updates code

---
+++ script/blackjack.js
@@ -0,0 +1,17 @@
+// const deal = document.getElementById('deal-button');
+// const playerCard1 = '';
+// const playerCard2 = document.getElementById('player-hand');
+// const dealerCard1 = document.getElementById('dealer-hand');
+// const dealerCard2 = document.getElementById('dealer-hand');
+
+
+const deal = document.getElementById('deal-button');
+    deal.addEventListener('click', function(e){
+        console.log(e);
+});
+
+const hit = document.getElementById('hit-button');
+hit.addEventListener('click', function(e){
+    console.log(e);
+});
+

933530e7a2c0fb0f046970d8a2a64c95bd65ce2e
NBESS <nicholasdbess@gmail.com>
2020-04-23 22:27:50 -0400

    finishes step 5 of exercise

--- script/blackjack.js
+++ script/blackjack.js
@@ -1,17 +1,29 @@
-// const deal = document.getElementById('deal-button');
-// const playerCard1 = '';
-// const playerCard2 = document.getElementById('player-hand');
-// const dealerCard1 = document.getElementById('dealer-hand');
-// const dealerCard2 = document.getElementById('dealer-hand');
-
-
 const deal = document.getElementById('deal-button');
-    deal.addEventListener('click', function(e){
-        console.log(e);
+
+    deal.addEventListener('click', function(){
+        const dealerHand = document.getElementById('dealer-hand')
+        let playerCard1 = document.createElement('img');
+        let playerCard2 = document.createElement('img');
+        dealerHand.appendChild(playerCard1);
+        dealerHand.appendChild(playerCard2);
+        playerCard1.setAttribute('src', 'resources/JC.jpg');
+        playerCard2.setAttribute('src', 'resources/6S.jpg');
+        const playerHand = document.getElementById('player-hand')
+        let dealerCard1 = document.createElement('img');
+        let dealerCard2 = document.createElement('img');
+        playerHand.appendChild(dealerCard1);
+        playerHand.appendChild(dealerCard2);
+        dealerCard1.setAttribute('src', 'resources/JC.jpg');
+        dealerCard2.setAttribute('src', 'resources/6S.jpg');
+
 });
 
 const hit = document.getElementById('hit-button');
-hit.addEventListener('click', function(e){
-    console.log(e);
+hit.addEventListener('click', function(){
+    const playerHand = document.getElementById('player-hand')
+    let playerCard = document.createElement('img');
+    playerHand.appendChild(playerCard);
+    playerCard.setAttribute('src', 'resources/2D.jpg');
+    
 });
 
